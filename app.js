57f6f903e1b0fbc9b3c0b84a96aa1dfd22537bea
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-10-23 14:06:31 +0530

    added change color function to allow the grid to be created

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const defaultGridSize = 16;
 let currentSize = defaultGridSize
 const grid = document.getElementById('container-grid')
 
+let mouseDown = false;
+
 const setCurrentSize = (newSize) => {
     currentSize = newSize
 };
@@ -12,7 +14,7 @@ function setupGrid(size) {
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
     for (let i = 0; i < size * size; i++) {
-        const gridElement = document.createElement('div')
+        const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element')
         gridElement.addEventListener('mouseover', changeColor)
         gridElement.addEventListener('mousedown', changeColor)
@@ -20,7 +22,19 @@ function setupGrid(size) {
     }
 }
 
-
+function changeColor(event) {
+    if (event.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        event.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+        event.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+        event.target.style.backgroundColor = '#fefefe'
+    }
+}
 
 window.onload = () => {
     setupGrid(defaultGridSize)
